af7a368de381812ac22ec07615f4bdb6d80185d7
Chase Whitener <cwhitener@gmail.com>
2018-04-05 21:32:27 -0400

    bump to 1.000, remove windows build complexity as that is now in Alien::libuv

--- .mm_awesome_header.pl
+++ .mm_awesome_header.pl
@@ -1,45 +1,6 @@
 use Config;
 use Alien::libuv ();
 
-# Some options behave differently on Windows
-sub WINLIKE () {
-    return 1 if $^O eq 'MSWin32';
-    # return 1 if $^O eq 'cygwin';
-    return 1 if $^O eq 'msys';
-    return '';
-}
-sub WINVER() {
-    return undef unless WINLIKE();
-    my $ver;
-    my $err = do {
-        local $@;
-        eval { require Win32; $ver = Win32::GetOSName(); 1; };
-        $@;
-    };
-    $ver = 'Win10' unless $ver; # just pick one if we couldn't find it
-    if ($err) {
-        warn "We had an error grabbing the Win32 OS Name: $err";
-    }
-
-    if ($ver =~ /^Win(?:Win32s|95|98|Me|NT4|NT3\.51|HomeSvr)/) {
-        warn "This version of Windows is really old and we can't install here";
-        exit(1);
-    }
-    return '0x0500' if $ver =~ /^Win2000/;
-    return '0x0501' if $ver =~ /^WinXP\/\.Net/;
-    return '0x0502' if $ver =~ /^Win2003/;
-    return '0x0600' if $ver =~ /^Win(?:Vista|2008)/;
-    return '0x0601' if $ver =~ /^Win7/;
-    return '0x0603' if $ver =~ /^Win8\.1/;
-    return '0x0602' if $ver =~ /^Win8/;
-    return '0x0A00' if $ver =~ /^Win10/;
-    return '0x0A00' if $ver =~ /^Win(?:2012|2014|2016)/; # not sure here
-
-    warn "We couldn't determine the version of Windows you're on.";
-    return undef;
-
-}
-
 # make sure we actually get stuff back from Alien::libuv
 sub TRIM {
     my $s = shift;
@@ -50,14 +11,7 @@ sub TRIM {
 }
 
 my @flags = ('-I.', $Config{ccflags});
-if (my $ver = WINVER()) {
-    push @flags, "-DWIN32_LEAN_AND_MEAN" unless $Config{ccflags} =~ /LEAN_AND_MEAN/;
-    push @flags, "-D_WINVER=$ver", "-D_WIN32_WINNT=$ver";
-    #push @flags, "-D_WIN32_WINDOWS=$ver";
-    #push @flags, "-D_WIN32_IE=$ver";
-}
 my $libs = Alien::libuv->libs();
-$libs .= ' -lpsapi -luserenv -lIphlpapi' if WINLIKE();
 {
     my $cflags = TRIM(Alien::libuv->cflags);
     my $cflags_s = TRIM(Alien::libuv->cflags_static);
@@ -69,16 +23,10 @@ $libs .= ' -lpsapi -luserenv -lIphlpapi' if WINLIKE();
         unshift @flags, $cflags_s if $cflags_s;
     }
 }
+
+# this will get merged into the meta
 my %xsbuild = (
     XSMULTI => 1,
-    # XSBUILD => {
-    #     xs => {
-    #         'lib/UV' => {
-    #           OBJECT => 'lib/UV$(OBJ_EXT) lib/perl_math_int64$(OBJ_EXT)',
-    #         },
-    #     },
-    # },
-    # OBJECT  => '$(O_FILES)',
     LIBS => [$libs],
     CCFLAGS => join(' ', @flags),
 );
